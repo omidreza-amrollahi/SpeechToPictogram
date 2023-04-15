91e9ba057f3af0e4c5f09a06717901086fc7a460
Omidreza Amrollahinasab <72734549+omidreza-amrollahi@users.noreply.github.com>
2023-04-15 15:58:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,176 +1,159 @@
-import os
 import whisper
-import streamlit as st
-from pydub import AudioSegment
-from model.clean_text import clean_text
-from model.POS_tagging import POS_tagging
+import gradio as gr 
+WhisperModels = ['tiny', 'base', 'small', 'medium', 'large']
 import matplotlib.pyplot as plt
+import matplotlib
 import requests
+matplotlib.use('AGG')
+import io
 from PIL import Image
+import PIL
 from io import BytesIO
 import openai
-openai.organization = "org-0etUm2oDAR2gC5nDs0EMyMWe"
-openai.api_key = "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"
+import os
+openai.organization = os.getenv('organization')
+openai.api_key = os.getenv('api_key')
 
-st.set_page_config(
-    page_title="Whisper based ASR",
-    page_icon="musical_note",
-    layout="wide",
-    initial_sidebar_state="auto",
-)
+def SpeechToText(audio, SelectedModel):
+    if audio == None : return "" 
+    model = whisper.load_model(SelectedModel)
+    audio = whisper.load_audio(audio)
+    audio = whisper.pad_or_trim(audio)
 
-audio_tags = {'comments': 'Converted using pydub!'}
+    # make log-Mel spectrogram and move to the same device as the model
+    mel = whisper.log_mel_spectrogram(audio).to(model.device)
 
-upload_path = "uploads/"
-download_path = "downloads/"
-transcript_path = "transcripts/"
+    # Detect the Max probability of language ?
+    _, probs = model.detect_language(mel)
+    lang = f"Language: {max(probs, key=probs.get)}"
 
-@st.cache()
-def to_mp3(audio_file, output_audio_file, upload_path, download_path):
-    ## Converting Different Audio Formats To MP3 ##
-    if audio_file.name.split('.')[-1].lower()=="wav":
-        audio_data = AudioSegment.from_wav(os.path.join(upload_path,audio_file.name))
-        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
+    #  Decode audio to Text
+    options = whisper.DecodingOptions(fp16 = False)
+    result = whisper.decode(model, mel, options)
+    return result.text, lang
 
-    elif audio_file.name.split('.')[-1].lower()=="mp3":
-        audio_data = AudioSegment.from_mp3(os.path.join(upload_path,audio_file.name))
-        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
+def clean_text(text):
+    """
+    we get rid of the commas and dots, maybe in the future there more things to get rid of in a sentence like !, ? ...
+    Args:
+        text (_type_): _description_
+    Returns:
+        _type_: _description_
+    """
+    text = text.lower()
+    text = text.replace(",", " ")
+    text = text.replace(".", " ")
+    text = text.replace("?", " ")
+    text = text.replace("-", " ")
+    text = text.split()
+    new_string = []
+    for temp in text:
+        if temp:
+            if temp == "i":
+                temp = "I"
+            new_string.append(temp)
+    concatString = ' '.join(new_string)
+    return new_string, concatString
 
-    elif audio_file.name.split('.')[-1].lower()=="ogg":
-        audio_data = AudioSegment.from_ogg(os.path.join(upload_path,audio_file.name))
-        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
+import nltk
+nltk.download('punkt')
+nltk.download('averaged_perceptron_tagger')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+nltk.data.path.append('/root/nltk_data')
+from nltk import pos_tag, word_tokenize
+from nltk.stem.wordnet import WordNetLemmatizer
 
-    elif audio_file.name.split('.')[-1].lower()=="wma":
-        audio_data = AudioSegment.from_file(os.path.join(upload_path,audio_file.name),"wma")
-        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
+class POS_tagging():
+    def __init__(self, concatString):
+        self.concatString = concatString
+    def handle_conjugation(self, tags):
+        # here we do the conjugation for verbs
+        new_sentence = []
+        for index, item in enumerate(tags):
+            if item[1] not in ['VBP', 'DT', 'IN', 'TO', 'VBG', 'VBD', 'VBN', 'VBZ']:
+                new_sentence.append(item[0])
+            elif item[1] in ['VBP', 'VBG', 'VBD', 'VBN', 'VBZ']:
+                new_verb = WordNetLemmatizer().lemmatize(item[0],'v')
+                if new_verb != "be":
+                    new_sentence.append(new_verb)
+        return new_sentence
+    def make_predictions(self):
+        tags = pos_tag(word_tokenize(self.concatString))
+        return self.handle_conjugation(tags)
 
-    elif audio_file.name.split('.')[-1].lower()=="aac":
-        audio_data = AudioSegment.from_file(os.path.join(upload_path,audio_file.name),"aac")
-        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
-
-    elif audio_file.name.split('.')[-1].lower()=="flac":
-        audio_data = AudioSegment.from_file(os.path.join(upload_path,audio_file.name),"flac")
-        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
-
-    elif audio_file.name.split('.')[-1].lower()=="flv":
-        audio_data = AudioSegment.from_flv(os.path.join(upload_path,audio_file.name))
-        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
-
-    elif audio_file.name.split('.')[-1].lower()=="mp4":
-        audio_data = AudioSegment.from_file(os.path.join(upload_path,audio_file.name),"mp4")
-        audio_data.export(os.path.join(download_path,output_audio_file), format="mp3", tags=audio_tags)
-    return output_audio_file
-
-@st.cache()
-def process_audio(filename, model_type):
-    model = whisper.load_model(model_type)
-    result = model.transcribe(filename, fp16=False)
-    return result["text"]
-
-@st.cache()
-def save_transcript(transcript_data, txt_file):
-    with open(os.path.join(transcript_path, txt_file),"w") as f:
-        f.write(transcript_data)
-
-def generate_pictogram(text_list):
+def generate_pic(text_to_search, ax):
     """
     we define a function here to use the api frpm arasaac, and return the image based on the text that we search
     ref: https://arasaac.org/developers/api
-
     Args:
         text_to_search (_type_): _description_
         ax (_type_): _description_
     """
-    text_length = len(text_list)
-    for i, col in enumerate(st.columns(text_length)):
-        with col:
-            search_url = f"https://api.arasaac.org/api/pictograms/en/bestsearch/{text_list[i]}"
-            search_response = requests.get(search_url)
-            search_json = search_response.json()
-            if search_json:
-                pic_url = f"https://api.arasaac.org/api/pictograms/{search_json[0]['_id']}?download=false"
-                pic_response = requests.get(pic_url)
-                img = Image.open(BytesIO(pic_response.content))
-                st.image(img, use_column_width='auto', caption=text_list[i])
-            else:
-                st.write(f"Using DALLE2 for /{text_list[i]}/")
-                try:
-                    response = openai.Image.create(
-                      prompt=text_list[i],
-                      n=2,
-                      size="512x512"
-                    )
-                    image_url = response['data'][0]['url']
-                    image_response = requests.get(image_url)
-                    img = Image.open(BytesIO(image_response.content))
-                    st.image(img, use_column_width='auto', caption=text_list[i])
-                except:
-                    st.write("Could not fetch images from the API")
-                # using SD for the purpose --> i do not like it
-                # SD_API_url = f"https://api.newnative.ai/stable-diffusion?prompt={text_list[i]}"
-                # SDresponse = requests.request("GET", SD_API_url)
-                # SDdata = SDresponse.json()
-                # SD_image_url = SDdata["image_url"]
-                # SDpic_response = requests.get(SD_image_url)
-                # img = Image.open(BytesIO(SDpic_response.content))
-                # st.image(img, use_column_width='auto', caption=text_list[i])
+    search_url = f"https://api.arasaac.org/api/pictograms/en/bestsearch/{text_to_search}"
+    search_response = requests.get(search_url)
+    search_json = search_response.json()
+    if search_json:
+        pic_url = f"https://api.arasaac.org/api/pictograms/{search_json[0]['_id']}?download=false"
+        pic_response = requests.get(pic_url)
+        img = Image.open(BytesIO(pic_response.content))
+        ax.imshow(img)
+        ax.set_title(text_to_search)
+    else:
+        try:
+            response = openai.Image.create(
+              prompt=text_to_search,
+              n=2,
+              size="512x512"
+            )
+            image_url = response['data'][0]['url']
+            image_response = requests.get(image_url)
+            img = Image.open(BytesIO(image_response.content))
+            ax.imshow(img)
+            ax.set_title(f"/{text_to_search}/")
+        except:
+            ax.set_title("Error!")
+    ax.axes.xaxis.set_visible(False)
+    ax.axes.yaxis.set_visible(False)
+    
+# we generate an initial pictogram
+# here we see the am, eating, an having a problem
+def save_pictogram(transcript):
+    """_summary_
+    Args:
+        text_list (_type_): _description_
+    """
+    cleaned_text, concatString = clean_text(transcript)
+    text_list = POS_tagging(concatString).make_predictions()
+    fig, ax = plt.subplots(1,len(text_list), figsize=(10,1.75))
+    if len(text_list) > 1:
+        for i, text in enumerate(text_list):
+            generate_pic(text, ax[i])
+    else:
+        for i, text in enumerate(text_list):
+            generate_pic(text, ax)
+    fig.savefig("pictogram.png")
+    safe_image = Image.open(r"pictogram.png")
+    return safe_image
 
+with gr.Blocks() as demo:
+    gr.Markdown("# Speech to Pictogram App")
+    gr.Markdown("The transcript of the audio can be in different languages, but the pictogram will only work for English")
+    gr.Markdown("The pictures which are titled in slashes (/) are generated using stable diffusion, however, the API here has a limit of 25 calls per minute.")
+    with gr.Row():
+        with gr.Column():
+            audio = gr.Audio(source="microphone", type="filepath")
+            dropdown = gr.Dropdown(label="Whisper Model", choices=WhisperModels, value='base')
+        with gr.Column():
+            transcript = gr.Textbox(label="Transcript")
+            lan = gr.Textbox(label="Language")
+        btn1 = gr.Button("Transcribe")
+        btn1.click(SpeechToText, inputs=[audio, dropdown], outputs=[transcript, lan])
+    #if lan.value == "Language: en":
+    image = gr.Image()
+    btn2 = gr.Button("Generate Pictogram")
+    btn2.click(save_pictogram, inputs=[transcript], outputs=[image])
+    gr.Markdown("Made by [Omidreza](https://github.com/omidreza-amrollahi)")
 
-
-st.title("Speech to Pictogram")
-st.info('Supports all popular audio formats - WAV, MP3, MP4, OGG, WMA, AAC, FLAC, FLV')
-uploaded_file = st.file_uploader("Upload audio file", type=["wav","mp3","ogg","wma","aac","flac","mp4","flv"])
-
-audio_file = None
-
-if uploaded_file is not None:
-    audio_bytes = uploaded_file.read()
-    with open(os.path.join(upload_path,uploaded_file.name),"wb") as f:
-        f.write((uploaded_file).getbuffer())
-    with st.spinner(f"Processing Audio ..."):
-        output_audio_file = uploaded_file.name.split('.')[0] + '.mp3'
-        output_audio_file = to_mp3(uploaded_file, output_audio_file, upload_path, download_path)
-        audio_file = open(os.path.join(download_path,output_audio_file), 'rb')
-        audio_bytes = audio_file.read()
-    print("Opening ",audio_file)
-    st.markdown("---")
-    col1, col2 = st.columns(2)
-    with col1:
-        st.markdown("Feel free to play your uploaded audio file")
-        st.audio(audio_bytes)
-    with col2:
-        whisper_model_type = st.radio("Please choose your model type", ('Tiny', 'Base', 'Small', 'Medium', 'Large'))
-
-    if st.button("Generate Transcript"):
-        with st.spinner(f"Generating Transcript..."):
-            transcript = process_audio(str(os.path.abspath(os.path.join(download_path,output_audio_file))), whisper_model_type.lower())
-
-            output_txt_file = str(output_audio_file.split('.')[0]+".txt")
-
-            save_transcript(transcript, output_txt_file)
-            output_file = open(os.path.join(transcript_path,output_txt_file),"r")
-            output_file_data = output_file.read()
-
-        st.write(f"Transcript: {output_file_data}")
-
-        if st.download_button(
-                             label="Download Transcript",
-                             data=output_file_data,
-                             file_name=output_txt_file,
-                             mime='text/plain'
-                         ):
-            st.balloons()
-            st.success('Download Successful !!')
-
-    if st.button("Generate Pictogram"):
-        with st.spinner(f"Generating Pictogram..."):
-            transcript = process_audio(str(os.path.abspath(os.path.join(download_path,output_audio_file))), whisper_model_type.lower())
-            cleaned_text, concatString = clean_text(transcript)
-            prediction = POS_tagging(concatString).make_predictions()
-            generate_pictogram(prediction)
-else:
-    st.warning('Please upload your audio file')
-
-st.markdown("<br><hr><center>Made by <a href='mailto:omidreza.ir@gmail.com?subject=Speech to pictogram WebApp!&body=Please specify the issue you are facing with the app.'><strong>Omidreza</strong></a> source code:<a href='https://github.com/omidreza-amrollahi'><strong>Github</strong></a></center><hr>", unsafe_allow_html=True)
-
-
+        
+demo.launch()
